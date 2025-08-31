e37ac6394b94d053feeb29f68981ac29a6162124
zulfa-lab <zulfanashihin@gmail.com>
2025-08-31 22:13:09 +0700

    Add files via upload

---
+++ Zulfa_project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zulfa_project1;
+
+/**
+ *
+ * @author zulfa
+ */
+public class Zulfa_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Zulfa Nashihin \nNPM : 2420506036 \nAlamat : Magelang \nNO HP : 1234567890");
+    }
+}
